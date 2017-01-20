5ecf5dca8f7edaedec03f0dbcdb27f268dd2847c
martinezpierre <pi.martinez@rubika-edu.com>
2017-01-20 22:19:31 +0100

    POWER OF DARWIIIIIIIIIN !!!!

---
+++ NoahArk/Assets/Scripts/AnimalInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimalInfo
+{
+    public List<string> name = new List<string>();
+
+    public string body = "";
+    public string head = "";
+    public string tail = "";
+    public string other = "";
+
+    public int weight = 0;
+}
---
+++ NoahArk/Assets/Scripts/BodyPartsPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartsPosition : MonoBehaviour
+{
+    public Transform headPosition;
+
+    public Transform tailPosition;
+
+    public List<Transform> otherPositions;
+}
